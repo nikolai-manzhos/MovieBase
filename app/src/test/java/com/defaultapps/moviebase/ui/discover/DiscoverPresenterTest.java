ea8b44f6a5c09bca401ae468c365c4148c6f6318
NikolayManzhos <NikolayManzhos@users.noreply.github.com>
2017-06-15 21:26:33 +0300

    Discover presenter unit test

---
+++ app/src/test/java/com/defaultapps/moviebase/ui/discover/DiscoverPresenterTest.java
@@ -0,0 +1,59 @@
+package com.defaultapps.moviebase.ui.discover;
+
+
+import com.defaultapps.moviebase.data.interactor.DiscoverUseCaseImpl;
+import com.defaultapps.moviebase.data.models.responses.genres.Genres;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.TestScheduler;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class DiscoverPresenterTest {
+
+    @Mock
+    DiscoverUseCaseImpl useCase;
+
+    @Mock
+    DiscoverContract.DiscoverView view;
+
+    private DiscoverPresenterImpl discoverPresenter;
+    private TestScheduler testScheduler;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        testScheduler = new TestScheduler();
+        discoverPresenter = new DiscoverPresenterImpl(useCase);
+        discoverPresenter.onAttach(view);
+    }
+
+    @Test
+    public void requestDataSuccess() throws Exception {
+        Genres genres = new Genres();
+        Observable<Genres> observable = Observable.just(genres).subscribeOn(testScheduler);
+        when(useCase.provideGenresList()).thenReturn(observable);
+
+        discoverPresenter.requestData();
+        testScheduler.triggerActions();
+
+        verify(view).showData(genres);
+    }
+
+    @Test
+    public void requestDataFailure() throws Exception {
+        Observable<Genres> observable = Observable.error(new Exception("zzzz"));
+        when(useCase.provideGenresList()).thenReturn(observable);
+
+        discoverPresenter.requestData();
+        verify(view, never()).showData(any());
+    }
+}
