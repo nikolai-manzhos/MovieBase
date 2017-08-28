82a2b79e6d3fa0585d6919fa9e733b9459219158
Nikolay <NikolayManzhos@users.noreply.github.com>
2017-08-28 11:18:58 +0300

    Remove bad view test

--- app/src/test/java/com/defaultapps/moviebase/ui/home/HomeViewTest.java
+++ app/src/test/java/com/defaultapps/moviebase/ui/home/HomeViewTest.java
@@ -1,19 +1,10 @@
 package com.defaultapps.moviebase.ui.home;
 
-import com.defaultapps.moviebase.BuildConfig;
-import com.defaultapps.moviebase.ui.main.MainActivity;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-import org.robolectric.shadows.support.v4.SupportFragmentTestUtil;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-@RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
 public class HomeViewTest {
 
     @Test
@@ -24,14 +15,5 @@ public class HomeViewTest {
         homeView.presenter = presenter;
         homeView.onRefresh();
         verify(presenter).requestMoviesData(true);
-    }
-
-    @Test
-    public void shouldDetachPresenterOnDestroyView() {
-        HomeViewImpl homeView = new HomeViewImpl();
-        SupportFragmentTestUtil.startFragment(homeView, MainActivity.class);
-        homeView.presenter = mock(HomePresenterImpl.class);
-        homeView.onDestroyView();
-        verify(homeView.presenter).onDetach();
     }
 }
