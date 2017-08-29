714c4ebef0da9c8fcb09876d094f130a021f015d
Nikolay <NikolayManzhos@users.noreply.github.com>
2017-08-29 22:19:20 +0300

    Remove empty presenter

--- app/src/main/java/com/defaultapps/moviebase/ui/user/UserPresenterImpl.java
+++
@@ -1,7 +0,0 @@
-package com.defaultapps.moviebase.ui.user;
-
-
-import com.defaultapps.moviebase.ui.base.BasePresenter;
-
-public class UserPresenterImpl extends BasePresenter<UserContract.UserView> implements UserContract.UserPresenter {
-}
