5016009caf3eee7152a949477f3481c66b047326
Nikolay <manzhos.nikolai@gmail.com>
2017-12-03 19:25:00 +0300

    Migrate to new Navigation, exclude few leaks, minor ui fixes

--- app/src/main/java/com/defaultapps/moviebase/ui/common/NavigationView.java
+++
@@ -1,7 +0,0 @@
-package com.defaultapps.moviebase.ui.common;
-
-import com.defaultapps.moviebase.ui.base.MvpView;
-
-public interface NavigationView extends MvpView {
-    void displayLoginActivity();
-}
